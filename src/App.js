eacc0471b58b7e7edbf106835fd9d6e8b35147f4
Newton Duarte <newton_@hotmail.com>
2021-11-30 10:06:46 -0300

    feat: add material-ui pickers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'date-fns';
+
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TimePicker from '@mui/lab/TimePicker';
+
 import './App.css';
 
 function App() {
+  const [time, setTime] = React.useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Material UI v5 - Pickers</h1>
+      <p>Time Picker - Only 0 - 30 minutes</p>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <TimePicker
+          label='Basic example'
+          value={time}
+          onChange={setTime}
+          inputProps={{ readOnly: true }}
+          renderInput={(params) => <TextField {...params} />}
+          shouldDisableTime={(timeValue, clockType) => {
+            if (
+              clockType === 'minutes' &&
+              timeValue !== 0 &&
+              timeValue !== 30
+            ) {
+              return true;
+            }
+
+            return false;
+          }}
+        />
+      </LocalizationProvider>
     </div>
   );
 }
